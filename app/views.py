71a186b9c736fbdc1081953f28d0a22eee6cd662
kr bindupriya <krbindupriya@gmail.com>
2023-05-20 13:13:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def srujana(request):
+    return HttpResponse('<h2>develoveper thinnavaa raa</h2>')
+def develoveper(request):
+    return HttpResponse('<h1><marquee>ammu ki phone cheyyaledaa develoveper</marquee></h1>')
+
